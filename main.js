be08a963ce320dd2dd56cf94c75f1a9a2cb7c805
amruthjithsunil <amruthjithsunil@gmail.com>
2022-12-04 04:29:48 +0530

    GetElementByTagName

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ console.log(title[0]);
 title[0].style.color = 'green';
 title[0].style.fontWeight = 'bold';*/
 
-let items = document.getElementsByClassName('list-group-item');
+/*let items = document.getElementsByClassName('list-group-item');
 console.log(items);
 console.log(items[0]);
 items[1].textContent = 'Hello 2';
@@ -18,4 +18,12 @@ items[2].style.backgroundColor = 'green';
 
 for(let i=0; i<items.length; i++){
     items[i].style.fontWeight = 'bold';
-}
+}*/
+
+let newItem = document.getElementsByClassName('new-list-item');
+console.log(newItem);
+newItem[0].style.fontWeight = 'bold';
+
+let items = document.getElementsByTagName('li');
+console.log(items[4]);
+items[4].style.color = 'green';
