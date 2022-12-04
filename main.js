221ae3c1ad7cabbe5e13540b7d0d957fd2f6b4e0
amruthjithsunil <amruthjithsunil@gmail.com>
2022-12-05 03:57:54 +0530

    Added del btn with fn and just added edit btn with no fn

--- main.js
+++ main.js
@@ -1,93 +1,34 @@
-/*let title = document.getElementsByClassName("title");
-console.log(title[0]);
-title[0].style.color = 'green';
-title[0].style.fontWeight = 'bold';*/
+let form = document.getElementById('addForm');
+let itemList = document.getElementById('items');
 
-/*let items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[0]);
-items[1].textContent = 'Hello 2';
-items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'yellow';
+form.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
 
-for(let i=0; i<items.length; i++){
-    items[i].style.backgroundColor = '#f4f4f4';
+function addItem(e){
+    e.preventDefault();
+    let newItem = document.getElementById('item').value;
+    let li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.appendChild(document.createTextNode(newItem));
+
+    let editBtn = document.createElement('button');
+    editBtn.className = 'btn btn-success btn-sm float-right';
+    editBtn.appendChild(document.createTextNode('edit'));
+    li.appendChild(editBtn);
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    deleteBtn.appendChild(document.createTextNode('X'));
+    li.appendChild(deleteBtn);
+
+    itemList.appendChild(li);
 }
 
-items[2].style.backgroundColor = 'green';
-
-for(let i=0; i<items.length; i++){
-    items[i].style.fontWeight = 'bold';
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are You Sure?')){
+            let li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
 }
-
-let newItem = document.getElementsByClassName('new-list-item');
-console.log(newItem);
-newItem[0].style.fontWeight = 'bold';
-
-let items = document.getElementsByTagName('li');
-console.log(items[4]);
-items[4].style.color = 'green';*/
-
-/*let item2 = document.querySelector('li:nth-child(2)');
-item2.style.backgroundColor = 'green';
-let item3 = document.querySelector('li:nth-child(3)');
-item3.style.visibility = 'hidden';
-
-let items = document.querySelectorAll('li');
-items[1].style.color = 'green';
-let oddItems = document.querySelectorAll('li:nth-child(odd)');
-for(let i=0; i<oddItems.length; i++){
-    oddItems[i].style.backgroundColor = 'green';
-}*/
-
-let itemList = document.querySelector('#items');
-/*console.log(itemList.parentNode);
-itemList.parentNode.style.backgroundColor = '#f4f4f4';
-console.log(itemList.parentNode.parentNode);*/
-
-/*console.log(itemList.parentElement);
-itemList.parentElement.style.backgroundColor = '#f4f4f4';
-console.log(itemList.parentElement.parentElement);*/
-
-//console.log(itemList.childNodes);
-
-/*console.log(itemList.children);
-console.log(itemList.children[1]);
-itemList.children[1].style.backgroundColor = 'lightblue';
-
-console.log(itemList.firstChild);
-console.log(itemList.firstElementChild);
-itemList.firstElementChild.textContent = 'Amruth';*/
-
-/*console.log(itemList.lastChild);
-console.log(itemList.lastElementChild);
-itemList.lastElementChild.textContent = 'Amruthjith';*/
-
-/*console.log(itemList.nextSibling);
-console.log(itemList.nextElementSibling);*/
-
-/*console.log(itemList.previousSibling);
-console.log(itemList.previousElementSibling);
-itemList.previousElementSibling.style.color = 'green';*/
-
-let newDiv = document.createElement('div');
-newDiv.className = 'hello';
-newDiv.id = 'hello1';
-newDiv.setAttribute('title','Hello Div');
-let newDivText = document.createTextNode('Hello World');
-newDiv.appendChild(newDivText);
-
-let newDiv1 = document.createElement('div');
-newDiv1.className = 'hello';
-newDiv1.id = 'hello1';
-newDiv1.setAttribute('title','Hello Div');
-let newDivText1 = document.createTextNode('Hello World');
-newDiv1.appendChild(newDivText1);
-
-let container = document.querySelector('header .container');
-let h1 = document.querySelector('header h1');
-let li = document.querySelector('.list-group-item');
-let container1 = document.querySelector('#items');
-console.log(newDiv);
-container.insertBefore(newDiv, h1);
-container1.insertBefore(newDiv1, li);
